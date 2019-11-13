1e1c3b037138bec485069573c170f57f5cd4570e
AntonKilk <ant.kilk@gmail.com>
2019-11-13 13:51:45 +0200

    2.2 I 2.3

--- task_2.2.js
+++ task_2.2.js
@@ -3,17 +3,13 @@ Write a function add with the following API.
 */
 
 function add(x, y) {
-  if (x == undefined) {
-    return function(y){
-      return (y); // how to pass two arguments instead of one?
-      };
-  }
-  else if (y == undefined) {
-    return function(y){
-      return x + y;
-      };
-  } 
-  else return x + y;     
+if (y == undefined) {
+  return function(y){
+    console.log(x);
+    console.log(y);
+    return x + y;
+    };
+  } else return x + y;     
   }
   
  console.log(add(1, 2) )   // 3
--- task_2.3.js
+++ task_2.3.js
@@ -4,19 +4,18 @@ Write a function double to duplicate each element in array in the sense of
  repeating it twice.
 */
 
-// function double(xs) {
-//     ???
-//   }
-  
-//   double([])     // []
-//   double([1])    // [1, 1]
-//   double([1, 2]) // [1, 1, 2, 2]
-// BUT this is a solution below isn't it?
-  let double = (xs) => {
-    return xs
-      .map(x => [x, x])
-      .reduce((zs, xs) => zs.concat(xs), [])
+function double(xs) {
+    xs.flatMap( x => [x,x]); //TypeError: xs.flatMap is not a function
   }
   
-  console.log(double([]))
-  console.log(double([4, 5])) 
+  double([])     // []
+  double([1])    // [1, 1]
+  double([1, 2]) // [1, 1, 2, 2]
+  // let double = (xs) => {
+  //   return xs
+  //     .map(x => [x, x])
+  //     .reduce((zs, xs) => zs.concat(xs), [])
+  // }
+  
+  // console.log(double([]))
+  // console.log(double([4, 5])) 
